cd04880997205262c98a698deab9c2355e033629
ngawate <ngawate@umich.edu>
2025-08-21 15:23:15 -0400

    logging and expection file adding

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+from logger import logging
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+
+    error_message = f'Error occured in python script name {exc_tb.tb_frame.f_code.co_filename}, \
+    line number {exc_tb.tb_lineno}, error message {str(error)}'
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail:sys):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
