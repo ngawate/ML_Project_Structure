54a02e9344f6ba96de27884f5e4d6aac4e2ccda4
ngawate <ngawate@umich.edu>
2025-08-23 13:09:15 -0400

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -13,6 +13,3 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     level=logging.INFO
 )
-
-if __name__ == "__main__":
-    logging.info("logging has started")
