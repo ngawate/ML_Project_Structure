9e1b965fe8ab18cb3b23c92850d3f393a1441c13
ngawate <ngawate@umich.edu>
2025-08-25 11:48:30 -0400

    final app.py

--- src/utils.py
+++ src/utils.py
@@ -40,3 +40,9 @@ def evaluate_model(X_train ,y_train, X_test, y_test, models):
         logging.error(f'{e} -- >{page_name}')
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
